b1c0d13930b988fed58b2011ddf9c47f8a9eb335
Aleks Gusev <algusb@mail.ru>
2022-10-01 20:43:59 +0300

    test passed

--- src/test/java/ru/netology/APICIPostmanEchoTest.java
+++ src/test/java/ru/netology/APICIPostmanEchoTest.java
@@ -20,7 +20,7 @@ public class APICIPostmanEchoTest {
                     .post("/post")
                     .then()
                     .statusCode(200)
-                    .body("data", equalTo("some value"))
+                    .body("data", equalTo("some data"))
             ;
         }
 
